dbdff0778aba477c70c1b5692799121ccd2fa0ac
kikebodi <bodi.inf@gmail.com>
2017-02-08 18:23:52 +0100

    Remove a thing...

--- src/main/java/com/kikebodi/main/ChallengeMain.java
+++ src/main/java/com/kikebodi/main/ChallengeMain.java
@@ -9,7 +9,7 @@ public class ChallengeMain {
 	public static final String easy = "e4820b45d2277f3844eac66c903e84be";
 	public static final String medium = "23170acc097c24edb98fc5488ab033fe";
 	public static final String hard = "665e5bcb0c20062fe8abaaf4628bb154";
-	public static final String anagram = "poultry outwits ants";
+	public static final String anagram = "hidden anagram";
 	
 	public static void main(String args[]) throws IOException, NoSuchAlgorithmException{
 		//Start performance test
